34fa48acf87220fb76abc561bf9984ed23abbf35
ritz078 <rkritesh078@gmail.com>
2020-07-27 17:11:43 +0530

    ga fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,35 @@
 import "../styles/main.scss";
 import React, { useEffect } from "react";
+import Head from "next/head";
 
-let ReactGA;
-if (typeof window !== "undefined") {
-  ReactGA = require("react-ga");
-  ReactGA.initialize(process.env.GA, {
-    debug: true,
-  });
-}
+const ID = process.env.GA;
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    if (typeof window === "undefined") return;
+    // @ts-ignore
+    window.dataLayer = window.dataLayer || [];
+    function gtag() {
+      // @ts-ignore
+      window.dataLayer.push(arguments);
+    }
+    // @ts-ignore
+    gtag("js", new Date());
+
+    // @ts-ignore
+    gtag("config", ID, { anonymize_ip: true });
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${ID}`}
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
