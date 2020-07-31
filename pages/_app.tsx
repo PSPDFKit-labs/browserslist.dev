6a833c362ce9a9dd529f60994f813a3ad31fab32
ritz078 <rkritesh078@gmail.com>
2020-07-31 15:13:40 +0530

    remove code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import "../styles/main.scss";
-import React, { useEffect } from "react";
+import React from "react";
 import Head from "next/head";
 
 const ID = process.env.GA;
@@ -7,21 +7,6 @@ const META_TAG_ID = process.env.META_TAG_ID;
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    if (typeof window === "undefined") return;
-    // @ts-ignore
-    window.dataLayer = window.dataLayer || [];
-    function gtag() {
-      // @ts-ignore
-      window.dataLayer.push(arguments);
-    }
-    // @ts-ignore
-    gtag("js", new Date());
-
-    // @ts-ignore
-    gtag("config", ID, { anonymize_ip: true });
-  }, []);
-
   return (
     <>
       <Head>
@@ -30,6 +15,17 @@ export default function MyApp({ Component, pageProps }) {
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${ID}`}
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', ${ID}, { 'anonymize_ip': true});
+          `,
+          }}
+        ></script>
         <link rel="shortcut icon" href="/favicon.ico" type="image/x-icon" />
       </Head>
       <Component {...pageProps} />
