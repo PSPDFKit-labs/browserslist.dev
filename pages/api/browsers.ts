cb68989e5a2e3f0659dd2f3bab6f04131b2c29c4
ritz078 <rkritesh078@gmail.com>
2021-07-23 21:27:46 +0530

    fixes

--- pages/api/browsers.ts
+++ pages/api/browsers.ts
@@ -2,6 +2,6 @@ import { NextApiRequest, NextApiResponse } from "next";
 import browserslist from "browserslist";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const x = browserslist(req.body);
-  res.status(200).json(x);
+  const browsers = browserslist(req.body);
+  res.status(200).json(browsers);
 }
