f6a36d4b3a2a708464dcf4350c6704c1b368465e
ritz078 <rkritesh078@gmail.com>
2021-07-26 13:53:44 +0530

    update caniuse-lite

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 const withSvgr = require("next-svgr");
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
 module.exports = (phase) =>
   withSvgr({
     env: {
       GA: process.env.GA,
       META_TAG_ID: process.env.META_TAG_ID,
-    },
-    experimental: {
-      optimizeFonts: phase !== PHASE_DEVELOPMENT_SERVER,
     },
   });
--- pages/api/browsers.ts
+++ pages/api/browsers.ts
@@ -2,6 +2,10 @@ import { NextApiRequest, NextApiResponse } from "next";
 import browserslist from "browserslist";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const browsers = browserslist(req.body);
-  res.status(200).json(browsers);
+  try {
+    const browsers = browserslist(req.body);
+    res.status(200).json(browsers);
+  } catch (e) {
+    res.status(404).send("Feature not found");
+  }
 }
