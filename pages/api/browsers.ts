160d708d0dacd53234b0a3d56b16b6ddedae4014
Ritesh Kumar <ritz078@users.noreply.github.com>
2023-06-12 12:55:36 +0530

    fixes

--- pages/api/browsers.ts
+++ pages/api/browsers.ts
@@ -1,9 +1,9 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import browserslist from "browserslist";
+import * as browserslist from "browserslist";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const browsers = browserslist(req.body);
+    const browsers = browserslist.default(req.body);
     res.status(200).json(browsers);
   } catch (e) {
     res.status(404).send("Feature not found");
