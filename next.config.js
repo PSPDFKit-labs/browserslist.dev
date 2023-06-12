0392d04060f55caf1162209695dc0fa69df30e71
Ritesh Kumar <ritz078@users.noreply.github.com>
2023-06-12 13:10:04 +0530

    fixes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 const withSvgr = require("next-svgr");
 
-module.exports = (phase) =>
-  withSvgr({
+module.exports = (phase) => {
+  return withSvgr({
     env: {
       GA: process.env.GA,
       META_TAG_ID: process.env.META_TAG_ID,
     },
+    transpilePackages: ["browserslist", "caniuse-lite"],
   });
+};
