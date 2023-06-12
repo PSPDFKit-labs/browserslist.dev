d93fbfd9fa69ac50079ad50d82c3cf44305a093f
Ritesh Kumar <ritz078@users.noreply.github.com>
2023-06-12 13:29:47 +0530

    fixes

--- pages/api/browsers.ts
+++ pages/api/browsers.ts
@@ -1,6 +1,5 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import browserslist from "browserslist";
-import * as lite from "caniuse-lite";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   try {
